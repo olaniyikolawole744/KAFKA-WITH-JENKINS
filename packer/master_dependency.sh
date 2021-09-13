698c1f6c567f770202faf5b5cdb97d4399d25e89
olaniyi <olaniyikolawole744@gmail.com>
2021-09-13 07:08:30 -0400

    push to repo

--- packer/master_dependency.sh
+++ packer/master_dependency.sh
@@ -16,3 +16,11 @@ chmod 755 ec2.ini
 touch /home/ec2-user/.ssh/id_rsa
 touch /home/ec2-user/.ssh/id_rsa.pub
 echo 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPU97b9uaJd7KZCdGrFIKeSnHh3cHKqOyST9Q/X4ZCEw 14809@LAPTOP-ND13IS36' >> /home/ec2-user/.ssh/id_rsa.pub
+echo '-----BEGIN OPENSSH PRIVATE KEY-----
+b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAAAMwAAAAtzc2gtZW
+QyNTUxOQAAACD1Pe2/bmiXeymQnRqxSCnkpx4d3Byqjskk/UP1+GQhMAAAAJhImd4dSJne
+HQAAAAtzc2gtZWQyNTUxOQAAACD1Pe2/bmiXeymQnRqxSCnkpx4d3Byqjskk/UP1+GQhMA
+AAAEBmG7i6VxhlPcfzTN3IM5yq/a1Pc9oyXU39zK/YcTs2rPU97b9uaJd7KZCdGrFIKeSn
+Hh3cHKqOyST9Q/X4ZCEwAAAAFTE0ODA5QExBUFRPUC1ORDEzSVMzNg==
+-----END OPENSSH PRIVATE KEY-----' >> /home/ec2-user/.ssh/id_rsa
+chmod 400 touch /home/ec2-user/.ssh/id_rsa
